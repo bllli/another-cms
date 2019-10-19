bd1662641f7fd48a10986bf7b1659ab2755a7db8
bllli <me@bllli.cn>
2019-10-19 21:56:15 +0800

    init

---
+++ src/main/java/cn/bllli/cms/anothercms/AnotherCmsApplication.java
@@ -0,0 +1,13 @@
+package cn.bllli.cms.anothercms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotherCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/bllli/cms/anothercms/AnotherCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bllli.cms.anothercms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
